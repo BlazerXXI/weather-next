631175db9eaec2914c7a276ee3de556c18c2b312
BlazerXXI <ruslandendik@gmail.com>
2024-09-11 22:25:47 +0300

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const nextConfig = withPWAInit({
+	dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swMinify: true,
+	disable: false,
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+});
+
+export default nextConfig({});
